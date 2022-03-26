f2d0ea7265c9c63d918984916eaf1d0eed3f4494
rubal501 <rubal820@gmail.com>
2022-03-25 19:29:33 -0600

    primer avance view registro restaurante

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,7 +1,25 @@
-<script setup></script>
+<script setup>
+import NavBar1 from '../components/NavBar.vue'
+
+
+
+</script>
+<script>
+
+import NavBar from '../components/NavBar.vue'
+import Footer from '../components/Footer.vue';
+export default{
+  components:{
+    NavBar,
+    Footer
+  }
+}
+</script>
 
 <template>
+<NavBar/>
   <main>
-    <h1>Hola primer proyecto</h1>
+    
   </main>
+  <Footer/>
 </template>
---
+++ front/src/views/RestaurantSignUp.vue
@@ -0,0 +1,57 @@
+<script>
+import NavBar from '../components/NavBar.vue'
+import Footer from '../components/Footer.vue'
+export default {
+    components:{
+        NavBar,
+        Footer
+    }
+}
+</script>
+
+<template>
+<NavBar/>
+<h1>Creacion de cuenta para restaurante</h1>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6">
+      <form>
+
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label>Nombre</label>
+      <input type="text" class="form-control"  placeholder="Nombre">
+    </div>
+    <div class="form-group col-md-6">
+      <label >Primer apellido </label>
+      <input type="text" class="form-control" placeholder="Primer apellido">
+    </div>
+    <div class="form-group col-md-6">
+      <label >Segundo apellido</label>
+      <input type="text" class="form-control"  placeholder="Nombre">
+    </div>
+    <div class="form-group col-md-6">
+      <label >Correo electronico</label>
+      <input type="email" class="form-control"  placeholder="Correo electronico" >
+    </div>
+    <div class="form-group col-md-6">
+      <label >Contrasena</label>
+      <input type="password" class="form-control" placeholder="Contrasena">
+    </div>
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Crear usuario</button>
+</form>  
+        </div>
+        <div class="col-lg-6">
+           Aqui segun yo va una imagen 
+        </div>
+    </div>
+</div>
+
+
+<Footer/>
+
+
+</template>
