7f60fc93327f2d6012229ac02ff2c1826ae503fc
Sebastián <alamina@ciencias.unam.mx>
2022-03-28 23:49:00 -0600

    Primer avance sobre la vista para el inicio de sesión.

---
+++ front/src/views/LoginView.vue
@@ -0,0 +1,64 @@
+<script>
+</script>
+
+<template>
+
+  <div id="login-box">
+
+    <section id="user-area">
+      <label>Usuario</label>
+      <input type="text" class="form-control">
+    </section>
+
+    <section id="pass-area">
+      <label>Contraseña</label>
+      <input type="text" class="form-control">
+    </section>
+
+    <section id="social-buttons">
+      <button>Facebook</button>
+      <button>Google</button>
+      <button>Apple</button>
+    </section>
+
+    <section id="forgotten-pass">
+      <a href="#">
+        Olvidé mi contraseña
+      </a>
+    </section>
+    
+    <section>
+      <button type="submit">Login</button>
+    </section>
+
+  </div>
+
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#login-box {
+  margin: auto;
+  width: 20%;
+  border: 3px solid black;
+  padding: 50px;
+}
+
+#user-area {
+  padding-bottom: 30px;
+}
+
+#pass-area {
+  padding-bottom: 60px;
+}
+
+#social-buttons {
+  padding-bottom: 10px;
+}
+
+#forgotten-pass {
+  padding-bottom: 20px;
+}
+
+</style>
