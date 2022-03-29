d52e75488f621d39d4b874bb8447f15f39764ab7
rubal501 <rubal820@gmail.com>
2022-03-29 13:14:06 -0600

    correcciones front

--- front/src/views/RestaurantSignUp.vue
+++ front/src/views/RestaurantSignUp.vue
@@ -1,20 +1,23 @@
 <script>
 import NavBar from '../components/NavBar.vue'
 import Footer from '../components/Footer.vue'
+import RestaurantForm from '../components/RestaurantForm.vue'
 
 export default {
     components:{
         NavBar,
-        Footer
+        Footer,
+        RestaurantForm
     },
     data(){
-      return{
+      return{ 
         formulario:{
           nombre : '',
-          email :'', 
-          firstSurename :'',
-          secondSurename :'',
-          password :'',
+          estado :'', 
+          calle :'',
+          cp : null,
+          numero: null,
+          municipio :'',
         },
         errores : [],
       }
@@ -38,37 +41,36 @@ export default {
               TODO
               }); */
         }
+        else{
+          console.log('los datos no jalaron')
+        }
       },
       checarFormulario: function (){
         this.errores=[]
+      
        if (!this.formulario.nombre) {
           this.errores.push('Nombre vacio')  
        }
-       if (!this.formulario.firstSurename) {
-          this.errores.push('Primer apellido vacio') 
+      if (!this.formulario.nombre) {
+          this.errores.push('Nombre vacio')  
        }
-      if (!this.formulario.secondSurename) {
-          this.errores.push('Segundo apellido vacio') 
+       if (!this.formulario.estado) {
+          this.errores.push('Estado vacio.') 
        }
-      if (!this.formulario.password) {
-          this.errores.push('Contrasena vacia') 
+      if (!this.formulario.calle) {
+          this.errores.push('Calle vacia.') 
        }
-      if (!this.validaEmail(this.formulario.email)) {
-          this.errores.push('El correo electronico no es valido')
-      }
+      if (!this.formulario.cp) {
+          this.errores.push('CP vacio') 
+       }  
+      if (!this.formulario.municipio) {
+          this.errores.push('Municipio vacio') 
+       }
       if (!this.errores.length){
         return true
       }
       console.log('errores en los datos')
-
-
-      },
-      validaEmail: function (email) {
-        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
       }
-
-
     }
 
 }
@@ -76,48 +78,22 @@ export default {
 
 <template>
 <NavBar/>
-<h1>Creacion de cuenta para restaurante</h1>
+<h1>Registro para restaurantes para restaurante</h1>
 
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-6">
-  <form @submit.prevent="submitForm">
-
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label>Nombre</label>
-      <input v-model="formulario.nombre"  type="text" class="form-control"  placeholder="Nombre">
-    </div>
-    <div class="form-group col-md-6">
-      <label >Primer apellido </label>
-      <input v-model="formulario.firstSurename" type="text" class="form-control" placeholder="Primer apellido">
-    </div>
-    <div class="form-group col-md-6">
-      <label >Segundo apellido</label>
-      <input v-model="formulario.secondSurename" type="text" class="form-control"  placeholder="Nombre">
-    </div>
-    <div class="form-group col-md-6">
-      <label >Correo electronico</label>
-      <input v-model="formulario.email" type="email" class="form-control"  placeholder="Correo electronico" >
-    </div>
-    <div class="form-group col-md-6">
-      <label >Contrasena</label>
-      <input v-model="formulario.password" type="password" class="form-control" placeholder="Contrasena">
-    </div>
-  </div>
-
-  <div v-if="errores.length != 0"  >
-    <b>Corrija los siguientes errores:
-    </b>
-    <ul>
-      <li  v-for="(error,i) in errores" :key="i" >
-        {{error}}
-      </li>
-    </ul>
-  </div>
-  
-  <button type="submit" class="btn btn-primary">Crear usuario</button>
-</form>  
+    <restaurant-form
+    :nombre="formulario.nombre"
+    :calle="formulario.calle"
+    :cp="formulario.cp"
+    :estado="formulario.estado"
+    :errores="errores"
+    :submitMethod="submitForm"
+    :numero="formulario.numero"
+    :municipio="formulario.municipio" 
+    />
+    
         </div>
         <div class="col-lg-6">
            Aqui segun yo va una imagen 
