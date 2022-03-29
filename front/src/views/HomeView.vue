d0c4156e389283c81a7c61bbfa08ac349076b8c8
Sebastián <alamina@ciencias.unam.mx>
2022-03-28 23:48:31 -0600

    La vista 'HomeView.vue' ahora muestra todas las views.

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -4,4 +4,9 @@
   <main>
     <h1>Hola primer proyecto</h1>
   </main>
+
+  <RouterLink v-for="route in this.$router.options.routes" :key="route.path" :to="route.path">
+    <div>{{route.name}}</div>
+  </RouterLink>
+
 </template>
