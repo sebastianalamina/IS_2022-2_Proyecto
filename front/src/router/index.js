99b188f1b661d924a7e4234cbaff7c058e5ca40c
Valencia <davidalencia@gmail.com>
2022-04-05 12:27:32 -0500

    router with redirect if no login

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,35 +1,52 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LoginView from '../views/LoginView.vue'
-import RestaurantSignUp from '../views/RestaurantSignUp.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import { useStore as useAuthStore } from "../stores/auth";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
+      meta: {
+        requiresAuth: false,
+      },
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import("../views/AboutView.vue").catch,
+      meta: {
+        requiresAuth: false,
+      },
     },
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue')
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
+      meta: {
+        requiresAuth: false,
+      },
     },
     {
-      path: '/restaurant-signup',
-      name: 'registro-restaurante',
-      component: () => import('../views/RestaurantSignUp.vue')
-    }
-  ]
-})
+      path: "/restaurant-signup",
+      name: "registro-restaurante",
+      component: () => import("../views/RestaurantSignUp.vue"),
+      meta: {
+        requiresAuth: false,
+      },
+    },
+  ],
+});
 
-export default router
+router.beforeEach((to) => {
+  const store = useAuthStore();
+
+  if (to.meta.requiresAuth && !store.isLogged) return "/login";
+});
+
+export default router;
