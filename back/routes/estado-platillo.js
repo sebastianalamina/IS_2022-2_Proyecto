3acb589a7e8ebd1909b4f2db58493b985040f493
Sebastián <alamina@ciencias.unam.mx>
2022-04-18 21:44:25 -0500

    Método POST sobre back 'estado-platillo' agregado.

--- back/routes/estado-platillo.js
+++ back/routes/estado-platillo.js
@@ -12,11 +12,11 @@ const Joi = require("joi");
 // Middleware.
 const validate = require('../utils/middleware/validate');
 
-/* Un GET requiere el ID de la orden ("identregadomicilio" o
+/* Este GET requiere el ID de la orden ("identregadomicilio" o
 "identregamesa"), un booleano para determinar si es entrega
 a domicilio (tabla "entregadomicilio") o entrega en mesa
-(tabla "entregamesa"), y devuelve el estado de la orden, así
-como el correspondiente "ordenenvio" o "ordennormal". */
+(tabla "entregamesa"), y devuelve la instancia correspondiente
+en formato JSON (lo cual contiene el estado de la orden). */
 router.get(
 	'/',
 	validate(Joi.object({
@@ -50,9 +50,60 @@ router.get(
 			where: { id_a_buscar: id_entrega }
 		});
 
-		// Debug:
+		// Debug temporal:
 		console.log(orden)
-		res.send("uwu");
+		res.json(orden);
+	}
+);
+
+/* Este POST requiere el ID de la orden ("identregadomicilio" o
+"identregamesa"), un booleano para determinar si es entrega
+a domicilio (tabla "entregadomicilio") o entrega en mesa
+(tabla "entregamesa"), el nuevo estado que se le aplicará
+a la orden, y actualiza el estado de dicha orden. Devuelve
+la nueva instancia en formato JSON. */
+router.post(
+	'/',
+	validate(Joi.object({
+		id_entrega: Joi.number().integer().required(),
+		es_domicilio: Joi.boolean().required(),
+		nuevo_estado: Joi.number()
+			.integer()
+			.min(0)
+			.max(3),
+	}), "query"),
+	async (req, res) => {
+
+		// Almacenamos las variables pasadas como parámetros.
+		let id_entrega = req.query.id_entrega;
+		let es_domicilio = req.query.es_domicilio;
+		let nuevo_estado = req.query.nuevo_estado;
+
+		// La tabla en la que Prisma buscará depende del tipo de orden.
+		let tabla_a_buscar = es_domicilio ? 'entregadomicilio' : 'entregamesa';
+		let id_a_buscar = es_domicilio ? 'identregadomicilio' : 'identregamesa';
+
+		// Verificamos que la orden se encuentre en la BD...
+		const id_entrega_count = await prisma.tabla_a_buscar.count({
+			where: { id_a_buscar: id_entrega },
+		});
+
+		// ...Si no, devolvemos el error correspondiente.
+		if (!id_entrega_count) {
+			return res.status(400).json({
+				error: `La tabla ${tabla_a_buscar} no contiene instancia de ID ${id_a_buscar} igual a ${id_entrega}`,
+			});
+		}
+
+		// ...Si sí, actualizamos la instancia correspondiente.
+		const orden_a_modificar = await prisma.tabla_a_buscar.update({
+			where: { id_a_buscar: id_entrega },
+			data: { estado: nuevo_estado }
+		});
+
+		// Debug temporal:
+		console.log(orden_a_modificar)
+		return res.status(201).json(orden_a_modificar);
 	}
 );
 
