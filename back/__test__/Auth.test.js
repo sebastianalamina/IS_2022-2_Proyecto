1e4473407b02790b182cb015860ca94b8efe4db7
Valencia <davidalencia@gmail.com>
2022-04-20 21:54:48 -0500

    tests skeletons

--- back/__test__/Auth.test.js
+++ back/__test__/Auth.test.js
@@ -1,16 +1,47 @@
 const request = require("supertest");
 const Joi = require("joi");
+const { faker } = require("@faker-js/faker");
 
 const app = require("../app");
 const validate = require("./utils/validate.js");
 
 describe("/auth", () => {
+  let usuario = faker.internet.email();
+  let password = faker.internet.password();
+
+  describe("POST /register", () => {
+    test("Respuesta correcta de usuario", function (done) {
+      request(app)
+        .post("/auth/register")
+        .send({
+          email: usuario,
+          contrasegna: password,
+          rol: "mesero",
+        })
+        .expect(200)
+        .expect(
+          validate(
+            Joi.object({
+              email: Joi.string(),
+            })
+          )
+        )
+        .end(done);
+    });
+    test("creación de administrador", () => {});
+    test("creación de cliente", () => {});
+    test("creación de cocinero", () => {});
+    test("creación de mesero", () => {});
+    test("creación de repartidor", () => {});
+    test("Middleware: permiso correcto", () => {});
+  });
+
   test("POST /login", function (done) {
     request(app)
       .post("/auth/login")
       .send({
-        email: "a@unam.mx",
-        contrasegna: "1",
+        email: usuario,
+        contrasegna: password,
       })
       .expect(200)
       .expect(
@@ -22,4 +53,5 @@ describe("/auth", () => {
       )
       .end(done);
   });
+  test("POST /rol", () => {});
 });
---
+++ back/__test__/utils/createUser.js
@@ -0,0 +1,21 @@
+const { PrismaClient } = require("@prisma/client");
+const { faker } = require("@faker-js/faker");
+
+const prisma = new PrismaClient();
+
+var user = {
+  _singleton: null,
+  getUser: function () {
+    if (!this._singleton) {
+      this._singleton = prisma.usuario.create({
+        data: {
+          email: faker.internet.email(),
+          contrasegna: faker.internet.password(),
+        },
+      });
+      return this._singleton;
+    }
+  },
+};
+
+module.exports = user;
