96b8cecda72704ad3791aaaa22877b6369c2c81a
Valencia <davidalencia@gmail.com>
2022-04-26 14:59:53 -0500

    fix seed

--- back/prisma/seed.js
+++ back/prisma/seed.js
@@ -1,192 +1,182 @@
-const { PrismaClient } = require("@prisma/client") ;
+const { PrismaClient } = require("@prisma/client");
 
 const { faker } = require("@faker-js/faker");
 
 const client = new PrismaClient();
 
-const roles = [
-            "CLIENTE",
-            "MESERO",
-            "COCINERO",
-            "ADMINISTRADOR",
-            "REPARTIDOR",
-        ]
+const roles = ["CLIENTE", "MESERO", "COCINERO", "ADMINISTRADOR", "REPARTIDOR"];
 
-async function seed(){ 
+async function seed() {
+  //Creamos las entradas referentes a los restaurantes
+  for (let i = 0; i < 3; i++) {
+    // Creamos 4 restaurantes
+    const id_restaurante = await client.restaurante.create({
+      data: {
+        nombre: faker.name.firstName(),
+        estado: faker.address.state(),
+        calle: faker.address.streetName(),
+        numero: 12,
+        cp: parseInt(faker.address.zipCode()),
+        municipio: faker.address.county(),
+      },
+      select: {
+        idrestaurante: true,
+      },
+    });
 
-    //Creamos las entradas referentes a los restaurantes
-    for (let i=0;i<3;i++){
-        // Creamos 4 restaurantes
-        const id_restaurante = await client.restaurante.create({
-            data:{
-                nombre : faker.name.firstName(),
-                estado : faker.address.state(),
-                calle : faker.address.streetName(),
-                numero : parseInt(faker.address.buildingNumber()),
-                cp : parseInt(faker.address.zipCode()),
-                municipio : faker.address.county(),
-            },
-            select:{
-                idrestaurante:true
-            }
-        });
+    const id_usuario_administrador = await client.usuario.create({
+      data: {
+        email: faker.internet.email(),
+        contrasegna: faker.internet.password(),
+        token: faker.random.alphaNumeric(20),
+        rol: roles[3],
+      },
+      select: {
+        idusuario: true,
+      },
+    });
 
-        const id_usuario_administrador = await client.usuario.create({
-            data:{
-                email : faker.internet.email(),
-                contrasegna : faker.internet.password(),
-                token : faker.random.alphaNumeric(20),
-                rol: roles[3],
-            },
-            select:{
-                idusuario : true
-            }
-        });
+    //Creamos el administrador del restaurante
+    const id_administrador = await client.administrador.create({
+      data: {
+        idusuario: id_usuario_administrador.idusuario,
+        idrestaurante: id_restaurante.idrestaurante,
+        nombre: faker.name.firstName(),
+        apatermo: faker.name.lastName(),
+        amaterno: faker.name.middleName(),
+      },
+      select: {
+        idadmin: true,
+      },
+    });
 
-        //Creamos el administrador del restaurante
-        const id_administrador = await client.administrador.create({
-            data:{
-                idusuario : id_usuario_administrador.idusuario,
-                idrestaurante : id_restaurante.idrestaurante,
-                nombre : faker.name.firstName(),
-                apatermo : faker.name.lastName(),
-                amaterno : faker.name.middleName(),
-            },
-            select:{
-                idadmin: true
-            }
-        });
+    //Creamos dos cocineros para el restaurante
+    for (let n = 0; n < 2; n++) {
+      const id_usuario_cocinero = await client.usuario.create({
+        data: {
+          email: faker.internet.email(),
+          contrasegna: faker.internet.password(),
+          token: faker.random.alphaNumeric(20),
+          rol: roles[2],
+        },
+        select: {
+          idusuario: true,
+        },
+      });
 
-        //Creamos dos cocineros para el restaurante
-        for( let n=0; n< 2; n++){
-
-            const id_usuario_cocinero = await client.usuario.create({
-                data : {
-                email : faker.internet.email(),
-                contrasegna : faker.internet.password(),
-                token : faker.random.alphaNumeric(20),
-                rol: roles[2]
-                },
-                select : {
-                    idusuario : true
-                }
-            })
-
-            await client.cocinero.create({
-                data:{
-                    idadmin : id_administrador.idadmin,
-                    idrestaurante : id_restaurante.idrestaurante,
-                    nombre : faker.name.firstName(),
-                    apaterno : faker.name.middleName(),
-                    amaterno : faker.name.lastName(),
-                    idusuario : id_usuario_cocinero.idusuario,
-                }
-            })
-        }
-
-        // Creamos tres meseros para el restaurante
-        for (let n=0; n< 3; n++){
-            const id_usuario_meseros = await client.usuario.create({ 
-                data : {
-                    email : faker.internet.email(),
-                    contrasegna : faker.internet.password(),
-                    token : faker.random.alphaNumeric(20),
-                    rol: roles[1]
-                    },
-                select : {
-                    idusuario : true
-                }
-            });
-
-            await client.mesero.create({
-                data : {
-                    idadmin : id_administrador.idadmin,
-                    idrestaurante : id_restaurante.idrestaurante,
-                    nombre : faker.name.firstName(),
-                    apatermo : faker.name.middleName(),
-                    amaterno : faker.name.lastName(),
-                    idusuario : id_usuario_meseros.idusuario,
-                }
-            });
-        }
-
-        for (let j=0; j< 2; j++){
-            // Creamos dos menus y le asignamos a cada uno 4 platillos.
-            const id_menu = await client.menu.create({
-                data:{
-                    idrestaurante : id_restaurante.idrestaurante,
-                },
-                select:{
-                    idmenu:true
-                }
-            });
-
-            for (let k=0; k< 4; k++){
-                // Creamos los cuatro platillos de cada uno de los menus
-                await client.platillo.create({
-                    data:{
-                        idmenu : id_menu.idmenu,
-                        idrestaurante : id_restaurante.idrestaurante,
-                        nombre : faker.commerce.productName(),
-                        costo : parseInt(faker.commerce.price(0,1000,0)),
-                    }
-                });
-            }
-        }
+      await client.cocinero.create({
+        data: {
+          idadmin: id_administrador.idadmin,
+          idrestaurante: id_restaurante.idrestaurante,
+          nombre: faker.name.firstName(),
+          apaterno: faker.name.middleName(),
+          amaterno: faker.name.lastName(),
+          idusuario: id_usuario_cocinero.idusuario,
+        },
+      });
     }
 
-    // Creamos 10 repartidores
+    // Creamos tres meseros para el restaurante
+    for (let n = 0; n < 3; n++) {
+      const id_usuario_meseros = await client.usuario.create({
+        data: {
+          email: faker.internet.email(),
+          contrasegna: faker.internet.password(),
+          token: faker.random.alphaNumeric(20),
+          rol: roles[1],
+        },
+        select: {
+          idusuario: true,
+        },
+      });
 
-    for (let i=0; i<10; i++){
-        const id_usuario_repartidor = await client.usuario.create({
-            data : {
-                email : faker.internet.email(),
-                contrasegna : faker.internet.password(),
-                token : faker.random.alphaNumeric(20),
-                rol: "REPARTIDOR",
-            },
-            select : {
-                idusuario : true,
-            }
-        });
-
-        await client.repartidor.create({
-            data : {
-                nombre : faker.name.firstName(),
-                apatermo : faker.name.middleName(),
-                amaterno : faker.name.findName(),
-                idusuario : id_usuario_repartidor.idusuario,
-            }
-        })
+      await client.mesero.create({
+        data: {
+          idadmin: id_administrador.idadmin,
+          idrestaurante: id_restaurante.idrestaurante,
+          nombre: faker.name.firstName(),
+          apatermo: faker.name.middleName(),
+          amaterno: faker.name.lastName(),
+          idusuario: id_usuario_meseros.idusuario,
+        },
+      });
     }
 
-    //Creamos 20 clientes
-    for (let i=0; i<20; i++){
-        const id_usuario_cliente = await client.usuario.create({
-            data : {
-                email : faker.internet.email(),
-                contrasegna : faker.internet.password(),
-                token : faker.random.alphaNumeric(20),
-                rol: "CLIENTE",
-            },
-            select : {
-                idusuario : true,
-            }
+    for (let j = 0; j < 2; j++) {
+      // Creamos dos menus y le asignamos a cada uno 4 platillos.
+      const id_menu = await client.menu.create({
+        data: {
+          idrestaurante: id_restaurante.idrestaurante,
+        },
+        select: {
+          idmenu: true,
+        },
+      });
+
+      for (let k = 0; k < 4; k++) {
+        // Creamos los cuatro platillos de cada uno de los menus
+        await client.platillo.create({
+          data: {
+            idmenu: id_menu.idmenu,
+            idrestaurante: id_restaurante.idrestaurante,
+            nombre: faker.commerce.productName(),
+            costo: parseInt(faker.commerce.price(0, 1000, 0)),
+          },
         });
-
-        await client.repartidor.create({
-            data : {
-                nombre : faker.name.firstName(),
-                apatermo : faker.name.middleName(),
-                amaterno : faker.name.findName(),
-                idusuario : id_usuario_cliente.idusuario,
-            }
-        })
+      }
     }
-    
+  }
 
-    console.log("Database sembrada")
+  // Creamos 10 repartidores
 
+  for (let i = 0; i < 10; i++) {
+    const id_usuario_repartidor = await client.usuario.create({
+      data: {
+        email: faker.internet.email(),
+        contrasegna: faker.internet.password(),
+        token: faker.random.alphaNumeric(20),
+        rol: "REPARTIDOR",
+      },
+      select: {
+        idusuario: true,
+      },
+    });
+
+    await client.repartidor.create({
+      data: {
+        nombre: faker.name.firstName(),
+        apatermo: faker.name.middleName(),
+        amaterno: faker.name.findName(),
+        idusuario: id_usuario_repartidor.idusuario,
+      },
+    });
+  }
+
+  //Creamos 20 clientes
+  for (let i = 0; i < 20; i++) {
+    const id_usuario_cliente = await client.usuario.create({
+      data: {
+        email: faker.internet.email(),
+        contrasegna: faker.internet.password(),
+        token: faker.random.alphaNumeric(20),
+        rol: "CLIENTE",
+      },
+      select: {
+        idusuario: true,
+      },
+    });
+
+    await client.repartidor.create({
+      data: {
+        nombre: faker.name.firstName(),
+        apatermo: faker.name.middleName(),
+        amaterno: faker.name.findName(),
+        idusuario: id_usuario_cliente.idusuario,
+      },
+    });
+  }
+
+  console.log("Database sembrada");
 }
 
 seed();
