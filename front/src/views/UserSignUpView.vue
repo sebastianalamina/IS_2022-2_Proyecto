1fdf60a671b53aa293143d14935c07865c9df4b0
Sebastián <alamina@ciencias.unam.mx>
2022-04-26 13:17:58 -0500

    Front de vista 'Registro usuario' actualizado.

--- front/src/views/UserSignUpView.vue
+++ front/src/views/UserSignUpView.vue
@@ -1,62 +1,30 @@
 <script>
 
-// Longitud del nombre de usuario.
-const MIN_ID_CLIENTE_LENGTH = 6
-const MAX_ID_CLIENTE_LENGTH = 15
-
 export default {
   data(){
     return{
       errors:[], // Arreglo de posibles errores del form.
-      idCliente:null,
-      idClienteError:null,
-      nombre:null,
-      nombreError:null,
-      aPaterno:null,
-      aPaternoError:null,
-      aMaterno:null,
-      aMaternoError:null,
       correo:null,
       correoError:null,
       password0:null,
       password0Error:null,
       password1:null,
       password1Error:null,
-      estado:null,
-      estadoError:null,
-      calle:null,
-      calleError:null,
-      numero:null,
-      numeroError:null,
-      cp:null,
-      cpError:null,
-      municipio:null,
-      municipioError:null,
+      rol:null,
+      rolError:null,
     }
   },
   methods:{
     checkForm:function(e) {
 
-      // Chequeo del nombre de usuario (el ID del cliente).
-      this.idClienteError = ""
-      if (!this.idCliente)
-        this.idClienteError = "Campo requerido."
-      else if (this.idCliente.length < MIN_ID_CLIENTE_LENGTH || this.idCliente.length > MAX_ID_CLIENTE_LENGTH)
-        this.idClienteError = `El nombre de usuario debe tener entre ${MIN_ID_CLIENTE_LENGTH} y ${MAX_ID_CLIENTE_LENGTH} caracteres.`
-
-      // Chequeo del nombre del usuario.
-      this.nombreError = ""
-      if (!this.nombre)
-        this.nombreError = "Campo requerido."
-
       // Chequeo del correo electrónico.
-      this.correoError = ""
+      this.correoError = null
       if (!this.correo)
         this.correoError = "Campo requerido."
 
       // Chequeo de la contraseña.
-      this.password0Error = ""
-      this.password1Error = ""
+      this.password0Error = null
+      this.password1Error = null
       if (!this.password0)
         this.password0Error = "Campo requerido."
       if (!this.password1)
@@ -64,10 +32,12 @@ export default {
       else if (this.password0 != this.password1)
         this.password1Error = "Las contraseñas no coinciden."
 
-      // TODO / TO-DO / TO DO
-      // El resto se determinará cuando la Base de Datos esté más actualizada.
+      // Chequeo del rol.
+      this.rolError = null
+      if (!this.rol)
+        this.rolError = "Campo requerido."
 
-      // ?
+      // Evita que se recargue la página.
       e.preventDefault();
     },
   }
@@ -79,30 +49,6 @@ export default {
 
   <div class="mitad izquierda">
     <form id="signup-box" @submit="checkForm"> <!-- action="/something" method="post"> -->
-
-      <section>
-        <label class="form-label">* Usuario</label>
-        <p class="error" v-if="idClienteError">{{idClienteError}}</p>
-        <input type="text" class="form-control" v-model="idCliente">
-      </section>
-
-      <section>
-        <label class="form-label">* Nombre</label>
-        <p class="error" v-if="nombreError">{{nombreError}}</p>
-        <input type="text" class="form-control" v-model="nombre">
-      </section>
-
-      <section>
-        <label class="form-label">Apellido Paterno</label>
-        <p class="error" v-if="aPaternoError">{{aPaternoError}}</p>
-        <input type="text" class="form-control" v-model="aPaterno">
-      </section>
-
-      <section>
-        <label class="form-label">Apellido Materno</label>
-        <p class="error" v-if="aMaternoError">{{aMaternoError}}</p>
-        <input type="text" class="form-control" v-model="aMaterno">
-      </section>
 
       <section>
         <label class="form-label">* Correo electrónico</label>
@@ -123,33 +69,15 @@ export default {
       </section>
 
       <section>
-        <label class="form-label">Estado</label>
-        <p class="error" v-if="estadoError">{{estadoError}}</p>
-        <input type="text" class="form-control" v-model="estado">
-      </section>
-
-      <section>
-        <label class="form-label">Calle</label>
-        <p class="error" v-if="calleError">{{calleError}}</p>
-        <input type="text" class="form-control" v-model="calle">
-      </section>
-
-      <section>
-        <label class="form-label">Número (creo que de calle)</label>
-        <p class="error" v-if="numeroError">{{numeroError}}</p>
-        <input type="text" class="form-control" v-model="numero">
-      </section>
-
-      <section>
-        <label class="form-label">Código postal</label>
-        <p class="error" v-if="cpError">{{cpError}}</p>
-        <input type="text" class="form-control" v-model="cp">
-      </section>
-
-      <section>
-        <label class="form-label">Municipio</label>
-        <p class="error" v-if="municipioError">{{municipioError}}</p>
-        <input type="text" class="form-control" v-model="municipio">
+        <label class="form-label">* Rol</label>
+        <p class="error" v-if="rolError">{{rolError}}</p>
+        <select class="form-control" v-model="rol">
+          <option>Administrador</option>
+          <option>Cliente</option>
+          <option>Cocinero</option>
+          <option>Mesero</option>
+          <option>Repartidor</option>
+        </select>
       </section>
       
       <input class="btn btn-secondary" type="submit" value="Registrarse">
