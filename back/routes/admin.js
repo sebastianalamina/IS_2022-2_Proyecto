04600777dc04026a7d16674613d4a1e30b5ea59f
rubal501 <rubal820@gmail.com>
2022-06-19 22:30:25 -0500

    la funcion para agregar empleados ya funciona de forma correcta

--- back/routes/admin.js
+++ back/routes/admin.js
@@ -18,30 +18,23 @@ router.get("/restaurante",
     bearerAuth,
     async (req, res) => {
         // Revisamos que el administrador tenga permisos de revisar la informacion de ese restaurante
+        console.log("id del suuari", req.user.idusuario)
 
-        let administrador;
-        try { // <- Issue #45 del repo.
-            console.log("el id del usuario es", req.user.idusuario);
-            administrador = await prisma.administrador.findFirst({
-                where:{
-                    idusuario : req.user.idusuario
-                },
-                select:{
-                    idadmin : true,
-                    idrestaurante : true
-                }
-            });
-        } catch (e) {
-            if (e.meta.cause === "Record to update not found.")
-                return res.status(404).send({ error: "registro no encontrado" });
-        }
+        const administrador = await prisma.administrador.findFirst({
+            where:{
+                idusuario: req.user.idusuario,
+            },
+            select:{
+                idusuario: true,
+                idrestaurante: true
+            }
+        }) 
+
+        console.log(administrador)
 
         if( administrador.idrestaurante ){
-            // Recabamos la informacion
-
-            let info;
-            try { // <- Issue #45 del repo.
-                info = await prisma.restaurante.findFirst({
+            // Recabamos la informacionj
+                const info = await prisma.restaurante.findFirst({
                     where:{
                         idrestaurante : administrador.idrestaurante
                     },
@@ -63,29 +56,41 @@ router.get("/restaurante",
                         }
                     }
                 });
-            } catch (e) {
-                if (e.meta.cause === "Record to update not found.")
-                    return res.status(404).send({ error: "registro no encontrado" });
-            }
-
-            res.json(info);
-        }
-
-       if(!administrador){
+                res.json(info)
+        }if(!administrador.idrestaurante){ 
             res.status(404).json({"message" : "El administrador no existe"});
-       }else{
-           const restaurante = await prisma.restaurante.findFirst({
-               where:{ idrestaurante : administrador.idrestaurante, },
-               include:{
-                   _count: { select: { mesero: true } },
-                    menu:{ select : { _count:{ select:{ platillo : true } } } },
-               }
-           });
-           res.json(restaurante); 
-       }
-        
+       }        
     }
 );
+
+// Esta ruta nos permite saber cuantas han sido las ordenes que se han pagado hasta ahora en el restaurante.
+
+router.get("/corte-caja",
+    esAdministrador,
+    bearerAuth,
+    async (req,res)=>{
+        // Revisamos que el administrador tenga permisos de revisar la informacion de ese restaurante
+        let administrador;
+        try { // <- Issue #45 del repo.
+            console.log("el id del usuario es", req.user.idusuario);
+            administrador = await prisma.administrador.findFirst({
+                where:{
+                    idusuario : req.user.idusuario
+                },
+                select:{
+                    idadmin : true,
+                    idrestaurante : true
+                }
+            });
+        } catch (e) {
+            if (e.meta.cause === "Record to update not found.")
+                res.status(404).send({ error: "registro no encontrado" });
+        }
+        if(administrador.idrestaurante){
+
+        }
+
+    });
 
 
 module.exports = router;
